6203a11d8745208fa08b56a1f97fced317ea9b14
= <sk8withaguitar@gmail.com>
2017-08-06 15:30:55 -0700

    Set request key to user-key and changed the API endpoint

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -9,12 +9,12 @@ require 'vcr'
 
 RSpec.configure do |c|
   c.add_setting :test_url, default: "http://www.google.com"
-  c.add_setting :default_api_url, default: "https://igdbcom-internet-game-database-v1.p.mashape.com"
+  c.add_setting :default_api_url, default: "https://api-2445582011268.apicast.io"
 end
 
 VCR.configure do |config|
   config.cassette_library_dir = "spec/fixtures/vcr_cassettes"
   config.allow_http_connections_when_no_cassette = true
   config.hook_into :webmock
-  config.filter_sensitive_data("<X-Mashape-Key>") { ENV["IGDB_API_KEY"] }
+  config.filter_sensitive_data("<user-key>") { ENV["IGDB_API_KEY"] }
 end
